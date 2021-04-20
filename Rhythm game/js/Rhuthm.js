5444180faf6519437953ceb89f19957a2e5ccdf0
yuhang41 <etc70129@gmail.com>
2021-04-20 15:26:22 +0800

    complete

--- Rhythm game/js/Rhuthm.js
+++ Rhythm game/js/Rhuthm.js
@@ -1,7 +1,9 @@
 let scoreBoard = document.querySelector(".score")
 let KeyTops = document.querySelectorAll(".top")
 let KeyBots = [...document.querySelectorAll(".bot")]
-
+let GoTop
+let TopLogo
+let TopIndex
 let timeUp = true
 let score = 0
 
@@ -22,38 +24,59 @@ KeyBotsProxy = new Proxy(status,{
         }
     }
 })
+
 function startGame(){//開始遊戲
     if(!timeUp) return
     showRandom()
     timeUp = false
+    setInterval(checkDep,10)
     this.style.display = "none"
     setTimeout(()=>{
         timeUp = true
         this.style.display = "block"
+        clearInterval(checkDep)
         alert('TimeOut')
-    },10000)
+    },20000)
 }
+
 function setScore(s){//計算分數
     score = s;
     scoreBoard.textContent = score
 }
+
 function showRandom(){ //動畫亂數索引和時間
     const KeyBot = Math.floor(Math.random() * KeyBots.length)
-    const time = Math.random() * (1500 - 1000) + 1000
+    const time = Math.random() * (1000 - 500) + 1500
     if(KeyBotsProxy[KeyBot]) return showRandom()
     setKetBots(KeyBot,time)
 }
+
 function setKetBots(KeyBot,time){ //哪幾個動畫啟動
     KeyBotsProxy[KeyBot] = true
     setTimeout(()=>{
         if(!timeUp) showRandom()
-    },500)
+    },1000)
     setTimeout(()=>{
         KeyBotsProxy[KeyBot] = false
     },time)
 }
-function playHandler(){
-    
+
+function playHandler(e){ //
+    KeyTops.forEach((top,index)=>{
+        if(top.dataset.key == e.keyCode){
+            TopIndex = index
+            GoTop = parseInt(window.getComputedStyle(KeyBots[index]).getPropertyValue("top"))
+            TopLogo = parseInt(window.getComputedStyle(top).getPropertyValue("top"))
+        }
+    })
 }
+
+function checkDep (){
+    // console.log(GoTop)
+    if(GoTop > 25 && GoTop < 90){
+        KeyBots[TopIndex].classList.remove('playing')
+    }
+}
+//案鍵問題，還是有問題
 document.querySelector('.start').addEventListener('click',startGame)
 window.addEventListener('keydown',playHandler)
